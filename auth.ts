a10be91654a0772654a84e7628d33ba9e7fccbe5
Oskar Kownacki <legromp123@gmail.com>
2026-03-27 09:55:42 +0100

    feat: update trustedOrigins handling in authentication configuration

--- auth.ts
+++ auth.ts
@@ -8,6 +8,9 @@ const db = client.db();
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGINS
+        ? process.env.BETTER_AUTH_TRUSTED_ORIGINS.split(",")
+        : [],
     emailAndPassword: {
         enabled: true,
     },
